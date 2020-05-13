422af9c5ad78ec51d2818554f1f7a6f9c5e76fe3
hollymein <hm917618@ohio.edu>
2020-05-13 16:41:25 -0700

    added code

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Holly Mein
+ *        Date: May 13, 2020
+ * Description: C++ program in git-lab-2 directory
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    
+    cout << "Hello Git!!" << endl;
+    return 0;
+    
+}// main
